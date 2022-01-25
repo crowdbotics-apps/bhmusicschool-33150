dcfb94acd68486e10992b490f096247fd627df1c
Crowdbotics <team@crowdbotics.com>
2022-01-25 04:36:27 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "bhmusicschool_33150",
+  url: "https://bhmusicschool_33150.botics.co",
+  api: "https://bhmusicschool_33150.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
